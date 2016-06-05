1bad938113ee6f4d745afe7753c941e5f9fe3d67
eviljoe <eviljoe@users.noreply.github.com>
2016-06-05 09:17:33 -0400

    [closes #69] Updated copyright year to 2016
    Updated the copyright year from  2015 to 2016.

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # EvilMusic - Web-Based Music Player
-# Copyright (C) 2015 Joe Falascino
+# Copyright (C) 2016 Joe Falascino
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- src/main/java/em/dao/song/SongInfoDAO.java
+++ src/main/java/em/dao/song/SongInfoDAO.java
@@ -1,5 +1,5 @@
 /*
- * EvilMusic - Web-Based Music Player Copyright (C) 2015 Joe Falascino
+ * EvilMusic - Web-Based Music Player Copyright (C) 2016 Joe Falascino
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
